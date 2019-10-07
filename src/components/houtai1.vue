299eda596dad122190bacdd738bb66517e3f9fcb
zly <760214212@qq.com>
2019-10-07 11:39:44 +0800

    卖家后台管理

---
+++ src/components/houtai1.vue
@@ -0,0 +1,123 @@
+<template>
+
+  <div>
+    <h1>卖家信息管理</h1>
+    <el-table
+      :data="seller"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="sid"
+        label="sid"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="cid"
+        label="cid"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="sname"
+        label="卖家姓名"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="username"
+        label="账号"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="password"
+        label="密码"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="provice"
+        label="省份"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="city"
+        label="城市"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="phone"
+        label="手机号"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="idcard"
+        label="身份证"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="sex"
+        label="性别"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱">
+      </el-table-column>
+      <el-table-column label="操作" >
+        <template slot-scope="users">
+          <el-button type="danger" round @click="todelete(users.row.id)" size="small">删除</el-button>
+          <el-button type="danger" round @click="toupdate(users.row.id)" size="small">修改</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+
+</template>
+
+<script>
+  import axios from 'axios'
+  export default{
+      data(){
+          return{
+              seller:[]
+          }
+      },
+    moutend(){
+
+    },
+    methods:{
+        query:function () {
+          var url="api/findSeller"
+          axios.get(url).then(res=> {
+            this.seller = res.data;
+
+          })
+    },
+      toupdate:function (id) {
+        this.$router.push('/updatehoutai1/'+id)
+      },
+
+      todelete: function (id) {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          var url = "api/deleteSeller"
+          axios.post(url, {id: id}).then(res => {
+            this.query();
+          })
+
+
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      },
+  }
+  }
+
+</script>
---
+++ src/components/updatehoutai1.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    车id ：<el-input v-model="seller.cid" placeholder="请输入账号"></el-input>
+    卖家姓名：<el-input  v-model="seller.sname" placeholder="请输入密码"></el-input>
+    卖家账号：<el-input  v-model="seller.username" placeholder="请输入邮箱"></el-input>
+    卖家密码：<el-input  v-model="seller.password" placeholder="请输入密码"></el-input>
+    省份：<el-input  v-model="seller.provice" placeholder="请输入密码"></el-input>
+    城市：<el-input  v-model="seller.city" placeholder="请输入密码"></el-input>
+    身份证号：<el-input  v-model="seller.idcard" placeholder="请输入密码"></el-input>
+    手机号：<el-input  v-model="seller.phone" placeholder="请输入密码"></el-input>
+    性别：<el-input  v-model="seller.sex" placeholder="请输入密码"></el-input>
+    邮箱：<el-input  v-model="seller.email" placeholder="请输入密码"></el-input>
+    <el-button type="primary" @click="update()">修改</el-button>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+  export default{
+      data(){
+          return{
+              seller:{
+                  sid:'',
+                cid:'',
+                sname:'',
+                username:'',
+                password:'',
+                provice:'',
+                city:'',
+                phone:'',
+                idcard:'',
+                sex:'',
+                email:'',
+              }
+          }
+      },
+    mounted(){
+          var id=this.$route.params.id
+          var url='api/findBySellerId'
+      axios.post(url,{id:id}).then(res=>{
+          if (res.data!=null){
+              this.seller=res.data();
+          }
+      })
+    },
+    methods:{
+        update:function () {
+          var url='api/updateSeller'
+          axios.post(url,this.seller).then(res=>{
+              this.$touter.push({name:'houtai1'})
+          })
+        },
+    }
+  }
+</script>
