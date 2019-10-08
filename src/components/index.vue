e270fa66c16c0a236aba4def6de0ce9976062680
Edmond_J <1398752619@qq.com>
2019-10-08 17:32:13 +0800

    添加主页

---
+++ src/components/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <div id="head">
+        <div><a href="" </div>
+
+
+      </div>
+
+
+    </div>
+</template>
+<script>
+    import axios from 'axios'
+    export default{
+        data(){
+            return {}
+        },
+        mounted(){
+
+        },
+        methods: {}
+    }
+</script>
+<style>
+#head{
+  position: relative;
+  z-index: 999;
+  width: 100%;
+  height: 35px;
+  line-height: 35px;
+  background: #fff;
+  color: #555;
+  border-bottom: 1px solid #ddd;
+}
+  #logo{
+
+  }
+</style>
